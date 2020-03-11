0aed0db82ad2217f0b32cfd7a024816929b03807
xiaobaocheng <604558835@qq.com>
2020-03-11 09:34:28 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: '肖宝成',
+    age: new Date().toLocaleDateString(),
+    count: 0,
+    animalArr: [{
+        id: 1,
+        name: 'cat',
+        age: 2
+      },
+      {
+        id: 2,
+        name: 'dog',
+        age: 3
+      },
+      {
+        id: 3,
+        name: 'fish',
+        age: 4
+      },
+      {
+        id: 4,
+        name: 'pig',
+        age: 5
+      },
+      {
+        id: 5,
+        name: 'chicken',
+        age: 6
+      }
+    ]
+  },
 
+  addHandleBtnClick() {
+    //错误做法：界面不会刷新
+    // this.data.count += 1; 
+    // console.log(this.data.count)
+
+    this.setData({
+      count:this.data.count+1
+    })
+  },
+
+
+  reduceHandleBtnClick() {
+    this.setData({
+      count: this.data.count - 1
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
